b8a1e49e04d192a57e2daf0a0f923b59208f23ec
Eric Conlon <ejconlon@gmail.com>
2012-02-25 12:44:21 -0800

    runjsv

---
+++ runjsv.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+var JSV = require("../JSV").JSV;
+var fs = require('fs');
+
+var schema_file = process.argv[2];
+var json_file = process.argv[3];
+
+console.log("Loading "+schema_file+" "+json_file);
+
+var schema_contents = fs.readFileSync(schema_file, 'utf8'); 
+var schema = JSON.parse(schema_contents); 
+
+var json_contents = fs.readFileSync(json_file, 'utf8'); 
+var json = JSON.parse(json_contents); 
+
+var env = JSV.createEnvironment();
+var report = env.validate(json, schema);
+
+if (report.errors.length === 0) {
+    //JSON is valid against the schema
+    console.log("PASS");
+} else {
+    console.log("FAIL:");
+    report.errors.forEach(function(error) {
+	console.log(error);
+    });
+}
+
+process.exit(report.errors.length);
--- validate.py
+++ validate.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
-import json
-from json_schema_validator.schema import Schema
-from json_schema_validator.validator import Validator
-
 import glob
 import re
+import copy
+import os
 from os.path import join as pjoin
+import json
+
+from json_schema_validator.schema import Schema
+from json_schema_validator.validator import Validator
 
 def read_json(filename):
     with open(filename) as f:
@@ -21,7 +23,29 @@ def get_casemap(directory):
         casemap[schema] = cases
     return casemap
 
-def run_testcases(casemap):
+def get_manual_casemap(directory):
+    manuals = glob.glob(pjoin(directory, "*.cases.txt"))
+    casemap = {}
+    for manual in manuals:
+        with open(manual) as f:
+            for line in f.readlines():
+                k, v = [pjoin(directory, x) for x in line.strip().split()]
+                globbed = glob.glob(v)
+                if len(globbed):
+                    if k not in casemap:
+                        casemap[k] = []
+                    casemap[k].extend(globbed)
+    return casemap
+
+def merge_casemaps(m1, m2):
+    m3 = copy.deepcopy(m1)
+    for k, vs in m2.iteritems():
+        if k not in m3:
+            m3[k] = []
+        m3[k].extend(vs)
+    return m3
+
+def run_testcases_python(casemap):
     for schemafile, casefiles in casemap.iteritems():
         print "SCHEMA:", schemafile
         sj = read_json(schemafile)
@@ -33,8 +57,20 @@ def run_testcases(casemap):
             print cj
             assert Validator.validate(schema, cj)
 
+def run_testcases_node(casemap):
+    for schemafile, casefiles in casemap.iteritems():
+        print "SCHEMA:", schemafile
+        for casefile in casefiles:
+            print "CASE:", casefile
+            assert os.system('./runjsv.js %s %s' % (schemafile, casefile)) == 0
+
+def run_testcases(casemap):
+    run_testcases_node(casemap)
+
 def main(directory):
-    casemap = get_casemap(directory)
+    auto_casemap = get_casemap(directory)
+    manual_casemap = get_manual_casemap(directory)
+    casemap = merge_casemaps(auto_casemap, manual_casemap)
     run_testcases(casemap)
 
 if __name__ == "__main__":
